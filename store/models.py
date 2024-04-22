52afd9da69b9b920801675e9a0d8da0de0203a61
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-04-22 22:08:01 +0530

    Add Project

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class Customer(models.Model):
@@ -10,7 +11,6 @@ class Customer(models.Model):
 
 	def __str__(self):
 		return self.name
-
 
 class Product(models.Model):
 	name = models.CharField(max_length=200)
